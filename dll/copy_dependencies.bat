3d7a7b824194a183bd351708431e9bc1f5587bd7
Torin <torinfrei@gmail.com>
2022-12-19 22:53:53 -0600

    updated copy_dependencies to use local GM installation rather than steamworks extension

--- dll/copy_dependencies.bat
+++ dll/copy_dependencies.bat
@@ -1,5 +1,5 @@
 @echo off
-Rem Copies needed .cpp & .h files from thirdparty/*
+Rem Copies ImGui into project and Runner headers
 Rem Sorry if this isn't the right way to do this
 Rem At least it works... right?
 
@@ -7,6 +7,10 @@ copy "thirdparty\imgui\im*.cpp"
 copy "thirdparty\imgui\im*.h"
 copy "thirdparty\imgui\backends\imgui_impl_dx11.*"
 
-copy "thirdparty\steamworks\source\Steamworks_vs\Steamworks\Extension_Interface.h"
-copy "thirdparty\steamworks\source\Steamworks_vs\Steamworks\YYRValue.h"
-copy "thirdparty\steamworks\source\Steamworks_vs\Steamworks\Ref.h"
+set program=GameMakerStudio2-Beta
+set runtime=runtime-2023.100.0.273
+set base="%PROGRAMDATA%\%program%\Cache\runtimes\%runtime%\yyc\include"
+
+copy "%base%\extension\YYRunnerInterface.h" "Extension_Interface.h"
+copy "%base%\YYRValue.h"
+copy "%base%\Ref.h"
