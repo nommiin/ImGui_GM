7a7c5f32db1a9056b6707e895a281629fcb328a3
Torin <torinfrei@gmail.com>
2023-01-04 10:34:58 -0600

    Add brief invoke as post-build event

--- tools/brief/main.js
+++ tools/brief/main.js
@@ -10,6 +10,8 @@ const Logger = require("./Logger");
  */
 const args = ["dll/", "extensions/ImGui_GM/ImGui_GM.yy", "scripts/ImGui/ImGui.gml"];
 try {
+    process.chdir("../");
+    
     const start = performance.now();
     if (Program.main(...args) === false) {
         throw `An unknown error has occured`;
